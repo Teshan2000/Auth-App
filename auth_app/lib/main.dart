ffce1942a851478cc0c9d34e53f8068e05b901e1
Teshan2000 <teshan_wijewardhana@outlook.com>
2025-11-06 23:08:22 +0530

    Stored Tokens
    User can login using stored key tokens, without relogging every time.

--- auth_app/lib/main.dart
+++ auth_app/lib/main.dart
@@ -2,13 +2,23 @@ import 'package:auth_app/screens/home.dart';
 import 'package:auth_app/screens/login.dart';
 import 'package:auth_app/screens/register.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final storage = FlutterSecureStorage();
+  
+  final accessToken = await storage.read(key: 'accessToken');
+  final refreshToken = await storage.read(key: 'refreshToken');
+  
+  runApp(MyApp(accessToken: accessToken, refreshToken: refreshToken));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, this.accessToken, this.refreshToken});
+
+  final String? accessToken;
+  final String? refreshToken;
   static final navigatorKey = GlobalKey<NavigatorState>();
 
   @override
@@ -17,15 +27,15 @@ class MyApp extends StatelessWidget {
       title: 'Auth App',
       debugShowCheckedModeBanner: false,
       navigatorKey: navigatorKey,
-      theme: ThemeData(
-        primaryColor: Colors.blue,
-      ),
+      theme: ThemeData(primaryColor: Colors.blue),
       initialRoute: '/',
       routes: {
-        '/': (context) => LoginPage(),
+         '/': (context) => accessToken != null
+            ? Home(accessToken: accessToken!, refreshToken: refreshToken ?? '')
+            : LoginPage(),
         'login': (context) => LoginPage(),
         'register': (context) => RegisterPage(),
-        'home': (context) => Home(accessToken: '', refreshToken: '',),
+        'home': (context) => Home(accessToken: accessToken ?? '', refreshToken: refreshToken ?? ''),
       },
     );
   }
--- auth_app/lib/services/authService.dart
+++ auth_app/lib/services/authService.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
 class AuthService {
   static const String baseUrl = "http://10.0.2.2:8080/api/auth";
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
 
   Future<Map<String, dynamic>> register(String email, String password) async {
     final response = await http.post(
@@ -27,10 +29,29 @@ class AuthService {
     );
 
     if (response.statusCode == 200) {
-      return jsonDecode(response.body);
+      final data = jsonDecode(response.body);
+
+      await _storage.write(key: 'accessToken', value: data['accessToken']);
+      await _storage.write(key: 'refreshToken', value: data['refreshToken']);
+
+      print("Tokens stored successfully!");
+      return data;
     } else {
       print("${response.body}, ${response.statusCode}");
       throw Exception("Failed to login: ${response.body}");
     }
+  }
+
+  Future<String?> getAccessToken() async {
+    return await _storage.read(key: 'accessToken');
+  }
+
+  Future<String?> getRefreshToken() async {
+    return await _storage.read(key: 'refreshToken');
+  }
+
+  Future<void> logOut() async {
+    await _storage.deleteAll();
+    print("Tokens cleareed!");
   }
 }
